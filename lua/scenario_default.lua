29facd55fad04f8f2202806da840a216231c62ee
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-06-12 05:07:52 +0600

    Update scenario_default.lua

--- lua/scenario_default.lua
+++ lua/scenario_default.lua
@@ -256,9 +256,9 @@ end)
 on_event("prestart", function()
 	local leaders = wesnoth.units.find_on_map { side = "3,4", canrecruit= true}
 	if #leaders < 2 then
-		create_timed_spawns(6, 11, 45, 5, 4, 21)
+		create_timed_spawns(6, 11, 30, 5, 4, 21)
 	else
-		create_timed_spawns(5, 11, 90, 4, 5, 23)
+		create_timed_spawns(5, 11, 70, 4, 5, 23)
 	end
 end)
 
